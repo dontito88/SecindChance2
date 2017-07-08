f271c1afc60682bfbc9fc38fbe6df3d09a90aa34
TrifonEstov <don.tito.g@gmail.com>
2017-07-08 13:04:18 +0300

    update

---
+++ SecondChancePart2/22.NestedDict03.UniqueNames/UniqueNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.NestedDict03.UniqueNames
+{
+    class UniqueNames
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var result = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                result.Add(input);
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ SecondChancePart2/22.NestedDict04.SortedCities/SortedCities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.NestedDict02CitiesByCountries
+{
+    class SortedCities
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var resultDict = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!resultDict.ContainsKey(continent))
+                {
+                    resultDict[continent] = new SortedDictionary<string, SortedSet<string>>();
+                }
+                if (!resultDict[continent].ContainsKey(country))
+                {
+                    resultDict[continent][country] = new SortedSet<string>();
+                }
+                resultDict[continent][country].Add(city);
+            }
+
+            foreach (var content in resultDict)
+            {
+                var continentName = content.Key;
+                var countryName = content.Value;
+                Console.WriteLine($"{continentName}: ");
+
+                foreach (var item in countryName)
+                {
+                    var country = item.Key;
+                    var city = item.Value;
+
+                    Console.WriteLine($"  {country} -> {string.Join(", ", city)}");
+                }
+            }
+        }
+    }
+}
