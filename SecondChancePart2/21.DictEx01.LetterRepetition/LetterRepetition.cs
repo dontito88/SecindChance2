451b654fc93c3c7ba1854533554eb5344549c557
TrifonEstov <don.tito.g@gmail.com>
2017-07-08 11:06:19 +0300

    update

---
+++ SecondChancePart2/21.DictEx01.LetterRepetition/LetterRepetition.cs
@@ -0,0 +1,33 @@
+﻿namespace ex01.LetterRepetition
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LetterRepetition
+    {
+        public static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            var lettersCount = new Dictionary<char, int>();
+
+            foreach (var letter in inputString)
+            {
+                if (!lettersCount.ContainsKey(letter))
+                {
+                    lettersCount[letter] = 0;
+                }
+                lettersCount[letter]++;
+            }
+
+            foreach (var letterCountPairs in lettersCount)
+            {
+                var letter = letterCountPairs.Key;
+                var count = letterCountPairs.Value;
+                Console.WriteLine($"{letter} -> {count}");
+            }
+        }
+    }
+}
